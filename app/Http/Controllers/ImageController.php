33adc62fb3a98b0d1f15961f508b9c49849c4c8e
poomani <poomani@zaigoinfotech.com>
2023-09-12 17:08:50 +0530

    image text

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -15,78 +15,76 @@ use ZipArchive;
 
 class ImageController extends Controller
 {
-    public function imageupload(Request $request,$id){
-        try{
+    public function imageupload(Request $request, $id)
+    {
+        try {
             $employee = User::find($id);
-            $employeeImages = EmployeeImages::with('images')->where('employee_id',$id)->orderBy('id','DESC')->get();
-            return view('image.image', compact('employee','employeeImages'));
-        }catch (\Exception $e) {
+            $employeeImages = EmployeeImages::with('images')->where('employee_id', $id)->orderBy('id', 'DESC')->get();
+            return view('image.image', compact('employee', 'employeeImages'));
+        } catch (\Exception $e) {
 
             return $e->getMessage();
         }
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $request->validate([
             'image' => 'required|image|mimes:jpeg,jpg,png,gif',
-          ]);
-          try{
-            if ($request->image != 'undefined') 
-        {
-            if($request->hasFile('image')) {
-
-                $image = Images::make($request->file('image'));
-    
-    
-                $imageName = time().'-'.$request->file('image')->getClientOriginalName();
-    
-                $destinationPath = public_path('images/');
-    
-                
-                $employee_details = User::with('userProfile')->find($request->emp_id);
-                $details = $employee_details->userProfile->first_name .''.$employee_details->userProfile->first_name.''.
-                            $employee_details->phone_number.''.$employee_details->userProfile->agency_name.'/n'.''.
-                            $employee_details->userProfile->city;
-                $image->text($details, 120, 100, function($font) {  
-    
-                      $font->size(35);  
-    
-                      $font->color('#668cff');  
-    
-                      $font->align('center');  
-    
-                      $font->valign('bottom');  
-    
-                      $font->angle(90);  
-    
-                });  
-    
-                $image->save($destinationPath.$imageName);
-                $data = new Image();
-                $data->name = $imageName;
-                $data->save();
-                $image = new EmployeeImages();
-                $image->employee_id = $request->emp_id;
-                $image->image_id = $data->id;
-                $image->image_name = $imageName;
-                $image->save(); 
-    
+        ]);
+        try {
+            if ($request->image != 'undefined') {
+                if ($request->hasFile('image')) {
+                    $image = Images::make($request->file('image'));
+                    $imageName = time() . '-' . $request->file('image')->getClientOriginalName();
+                    $destinationPath = public_path('images/');
+                    $max_len     = 20;
+                    $width       = 600;
+                    $height      = 300;
+                    $center_x    = $width / 2;
+                    $center_y    = $height / 2;
+                    $font_height = 20;
+                    $font_size   = 15;
+                    $employee_details = User::with('userProfile')->find($request->emp_id);
+                    $details = $employee_details->userProfile->first_name . ',' .
+                        $employee_details->phone_number . ',' . $employee_details->userProfile->agency_name . ',' . '' .
+                        $employee_details->userProfile->city;
+                    $lines = explode("\n", wordwrap($details, $max_len));
+                    $y     = $center_y - ((count($lines) - 1) * 20);
+                    // $image   = \Image::canvas($width, $height);
+                    foreach ($lines as  $line) {
+                        $image->text($line, $center_x, $y, function ($font) use ($font_size) {
+                            $font->file(public_path('fonts/Roboto-Regular.ttf'));
+                            $font->size($font_size);
+                            $font->color('#FF0000');
+                            $font->align('center');
+                            $font->valign('center');
+                        });
+                        $y += $font_height  * 2;
+                    }
+                    $image->save($destinationPath . $imageName);
+                    $data = new Image();
+                    $data->name = $imageName;
+                    $data->save();
+                    $image = new EmployeeImages();
+                    $image->employee_id = $request->emp_id;
+                    $image->image_id = $data->id;
+                    $image->image_name = $imageName;
+                    $image->save();
+                }
             }
-              
-        }
-        return redirect()->back()->withSuccess(' Image added successfully.');
-          }catch (\Exception $e) {
+            return redirect()->back()->withSuccess(' Image added successfully.');
+        } catch (\Exception $e) {
             return $e->getMessage();
         }
     }
-    public function destroy($id){
-            EmployeeImages::where('image_id',$id)->delete();
-            Image::find($id)->delete();
-            return redirect()->back()->withSuccess(' Image deleted successfully.');
-        
+    public function destroy($id)
+    {
+        EmployeeImages::where('image_id', $id)->delete();
+        Image::find($id)->delete();
+        return redirect()->back()->withSuccess(' Image deleted successfully.');
     }
     public function downloadImage(Request $request)
-        {
-           return redirect()->back();
-
-            }          
+    {
+        return redirect()->back();
+    }
 }
